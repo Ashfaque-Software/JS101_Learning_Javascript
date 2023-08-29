ce51ac67f7177286d4ca764e82282fc7ab444943
Ashfaque-Software <ashfaqueansari101@gmail.com>
2023-08-29 12:59:30 +0530

    added Mathematical_Operator .code

---
+++ Mathematical_Operator.js
@@ -0,0 +1,19 @@
+let  N=10
+ {
+   
+   
+const square = N ** 2;
+console.log(square);
+
+}
+
+
+
+let one=1;
+let two=2;
+let three=3;
+let four=4;
+let five=5;
+
+ console.log(one*two*three*four*five)
+}
